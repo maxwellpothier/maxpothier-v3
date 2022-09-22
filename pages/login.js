bf7b6d67b043f1d3bf55ef658089777692c5661b
maxwellpothier <pothiermaxwell@gmail.com>
2022-09-21 21:30:57 -0600

    creates forms for signup and login

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,29 @@
+import { useForm } from "react-hook-form";
+
+import styles from "./login.module.scss";
+
 const Login = () => {
+	const hookForm = useForm();
+
+	const onSubmit = ({username, password}) => {
+		console.log(username, password);
+	};
+
 	return (
 		<div>
 			Login Page
+			<form onSubmit={hookForm.handleSubmit(onSubmit)} className={styles.loginForm}>
+				<input
+					{...hookForm.register("username")}
+					placeholder={"Username"}
+				/>
+				<input
+					{...hookForm.register("password")}
+					placeholder={"Password"}
+					type={"password"}
+				/>
+				<button type={"submit"}>Log In</button>
+			</form>
 		</div>
 	);
 };
--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,37 @@
+import {useForm} from "react-hook-form";
+
+import styles from "./signup.module.scss";
+
 const Signup = () => {
+	const hookForm = useForm();
+
+	const onSubmit = ({username, firstName, lastName, password}) => {
+		console.log(username, firstName, lastName, password);
+	};
+
 	return (
 		<div>
 			Signup Page
+			<form onSubmit={hookForm.handleSubmit(onSubmit)} className={styles.signupForm}>
+				<input
+					{...hookForm.register("username")}
+					placeholder={"Username"}
+				/>
+				<input
+					{...hookForm.register("firstName")}
+					placeholder={"First Name"}
+				/>
+				<input
+					{...hookForm.register("lastName")}
+					placeholder={"Last Name"}
+				/>
+				<input
+					{...hookForm.register("password")}
+					placeholder={"Password"}
+					type={"password"}
+				/>
+				<button type={"submit"}>Sign Up</button>
+			</form>
 		</div>
 	);
 };
