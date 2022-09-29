b798729cb3411894e888ad559aa338826d26088c
maxwellpothier <pothiermaxwell@gmail.com>
2022-09-28 21:33:33 -0600

    starts to wire up the posts to be displayed

--- components/PostsList.js
+++ components/PostsList.js
@@ -1,6 +1,11 @@
+import { usePosts } from "../lib/hooks";
 import styles from "./postsList.module.scss";
 
 const PostsList = () => {
+	const {posts} = usePosts();
+
+	console.log(posts);
+
 	return (
 		<div className={styles.postsListContainer}>
 			Posts List
