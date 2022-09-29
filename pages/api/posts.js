470ce60c0caf1792cd76095d0ce4ae4aef434f78
maxwellpothier <pothiermaxwell@gmail.com>
2022-09-28 22:48:06 -0600

    fixing api call

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,7 +2,7 @@ import prisma from "../../lib/prisma";
 import {validateRoute} from "../../lib/auth";
 
 export default validateRoute(async (req, res) => {
-	const posts = await prisma.posts.findMany({});
+	const posts = await prisma.post.findMany({});
 
 	res.json(posts);
 });
