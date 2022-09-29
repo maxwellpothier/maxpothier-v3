b851a0d89fc3b4b5aa477cd4dc2d508c65d1faaa
maxwellpothier <pothiermaxwell@gmail.com>
2022-09-28 22:31:08 -0600

    Just... wow

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,8 +1,8 @@
 import prisma from "../../lib/prisma";
 import {validateRoute} from "../../lib/auth";
 
-export default validateRoute(async(req, res) => {
+export default validateRoute(async (req, res) => {
 	const posts = await prisma.posts.findMany({});
 
-	res.json(playlists);
+	res.json(posts);
 });
