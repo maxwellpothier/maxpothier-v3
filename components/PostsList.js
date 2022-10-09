b9c0996a84a3c0ec8ac726a22c9d3121d72b822a
maxwellpothier <pothiermaxwell@gmail.com>
2022-10-09 16:30:40 -0600

    connecting the seeded data to the project

--- components/PostsList.js
+++ components/PostsList.js
@@ -1,7 +1,18 @@
 import PostCard from "./PostCard";
 import styles from "./postsList.module.scss";
+import prisma from "../lib/prisma";
+import { useEffect } from "react";
 
 const PostsList = () => {
+
+	useEffect(() => {
+		(async () => {
+			const posts = await prisma.post.findMany();
+
+			console.log(posts);
+		})();
+	});
+
 	return (
 		<div className={styles.postsListContainer}>
 			<PostCard/>
