99fda6e528afb533c6853283993d146bda26ba15
maxwellpothier <pothiermaxwell@gmail.com>
2022-10-13 08:36:51 -0600

    middleware to protect pages when token is found

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,12 +6,25 @@ const loggedInPages = [
 	"/profile"
 ];
 
+const loggedInRestricted = [
+	"/signup",
+	"/login",
+];
+
 export default function middleware(req) {
 	if (loggedInPages.find(p => p === req.nextUrl.pathname)) {
 		const token = req.cookies.WT_ACCESS_TOKEN;
 
 		if (!token) {
 			return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+		}
+	}
+
+	if (loggedInRestricted.find(p => p === req.nextUrl.pathname)) {
+		const token = req.cookies.WT_ACCESS_TOKEN;
+
+		if (token) {
+			return NextResponse.redirect(`${req.nextUrl.origin}/`);
 		}
 	}
 };
