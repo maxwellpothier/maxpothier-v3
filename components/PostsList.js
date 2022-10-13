f27352925b699346ededac38a569576772f4e256
maxwellpothier <pothiermaxwell@gmail.com>
2022-10-12 19:57:29 -0600

    lining up with main for PR

--- components/PostsList.js
+++ components/PostsList.js
@@ -1,18 +1,7 @@
 import PostCard from "./PostCard";
 import styles from "./postsList.module.scss";
-import prisma from "../lib/prisma";
-import { useEffect } from "react";
 
 const PostsList = () => {
-
-	useEffect(() => {
-		(async () => {
-			const posts = await prisma.post.findMany();
-
-			console.log(posts);
-		})();
-	});
-
 	return (
 		<div className={styles.postsListContainer}>
 			<PostCard/>
