72a3473ba27bd146d27e3d3c44c82fdf10dcb372
maxwellpothier <pothiermaxwell@gmail.com>
2022-10-16 00:15:00 -0600

    creates api endpoint for getting posts!!!!

--- components/PostsList.js
+++ components/PostsList.js
@@ -1,7 +1,11 @@
 import PostCard from "./PostCard";
 import styles from "./postsList.module.scss";
+import {usePost} from "../lib/hooks";
 
 const PostsList = () => {
+	const {posts} = usePost();
+
+	console.log(posts);
 	return (
 		<div className={styles.postsListContainer}>
 			<PostCard/>
--- pages/api/post.js
+++ pages/api/post.js
@@ -1,3 +1,15 @@
-export default function handler(req, res) {
+import prisma from "../../lib/prisma";
+import {validateRoute} from "../../lib/auth";
 
-};
+export default validateRoute(async (req, res) => {
+	const posts = await prisma.post.findMany({
+		include: {
+			music: true,
+		},
+		orderBy: {
+			updatedAt: "desc"
+		}
+	});
+
+	res.json(posts);
+});
